8dd58e9b2914432b11a78ff6649b348af712edab
auniquesun <Mr.sunhy@outlook.com>
2022-06-09 21:32:56 +0800

    check total number of parameters

--- test.py
+++ test.py
@@ -7,33 +7,55 @@ from perceiver.model.image import ImageInputAdapter
 from perceiver.model.pointcloud import PointCloudInputAdapter
 
 
-# ------ Image
-# Fourier-encodes pixel positions and flatten along spatial dimensions
-input_adapter = ImageInputAdapter(
-  image_shape=(224, 224, 3),  # M = 224 * 224
-  num_frequency_bands=64,
-)
+# # ------ Image
+# # Fourier-encodes pixel positions and flatten along spatial dimensions
+# input_adapter = ImageInputAdapter(
+#   image_shape=(224, 224, 3),  # M = 224 * 224
+#   num_frequency_bands=64,
+# )
 
-# Generic Perceiver encoder
-encoder = PerceiverEncoder(
-  input_adapter=input_adapter,
-  num_latents=512,  # N
-  num_latent_channels=256,  # D
-  num_cross_attention_qk_channels=input_adapter.num_input_channels,  # C
-  num_cross_attention_heads=1,
-  num_self_attention_heads=8,
-  num_self_attention_layers_per_block=6,
-  num_self_attention_blocks=8,
-  dropout=0.0,
-)
+# # Generic Perceiver encoder
+# encoder = PerceiverEncoder(
+#   input_adapter=input_adapter,
+#   num_latents=512,  # N
+#   num_latent_channels=256,  # D
+#   num_cross_attention_qk_channels=input_adapter.num_input_channels,  # C
+#   num_cross_attention_heads=1,
+#   num_self_attention_heads=8,
+#   num_self_attention_layers_per_block=6,
+#   num_self_attention_blocks=8,
+#   dropout=0.0,
+# )
 
-# 怎么输入数据呢，其实就传个 (batch_size, 224,224,3) 的张量就行吧
-input = torch.randn(2, 224, 224, 3)
+# input = torch.randn(2, 224, 224, 3)
 # output: [2, 512, 512]
-output = encoder(input)
-# results: [2, 512]
-results = output.mean(1)
-print(results.shape)
+# output = encoder(input)
+# print(output.shape)
+
+# from torch.utils.data import DataLoader
+# from datasets.data import ShapeNetRender
+# from utils import transform
+
+# ------ test PointCloud dataloader
+# train_set = ShapeNetRender(transform)
+# train_loader = DataLoader(
+#         train_set,
+#         batch_size=4,
+#         shuffle=True,
+#         num_workers=0,
+#         pin_memory=True,
+#         drop_last=False
+#     )
+
+# for i, ((pc_t1, pc_t2), imgs) in enumerate(train_loader):
+#     batch_size = pc_t1.shape[0]
+#     pc_t1, pc_t2, imgs = pc_t1.to(0), pc_t2.to(0), imgs.to(0)
+#     imgs = torch.permute(imgs, (0, 2, 3, 1))
+#     print('batch_size:', batch_size)
+#     print('pc_t1.shape:', pc_t1.shape)
+#     print('pc_t2.shape:', pc_t2.shape)
+#     print('imgs.shape:', imgs.shape)
+#     break
 
 # ------ PointCloud
 input_adapter_pc = PointCloudInputAdapter(
@@ -58,6 +80,7 @@ encoder_pc = PerceiverEncoder(
 input_pc = torch.randn(2, 2048, 3)
 # output_pc: [2, 128, 512]
 output_pc = encoder_pc(input_pc)
-# results_pc: [2, 512]
-results_pc = output_pc.mean(1)
-print(results_pc.shape)
+print(output_pc.shape)
+
+pytorch_total_params = sum(p.numel() for p in encoder_pc.parameters() if p.requires_grad)
+print('total parameters:', pytorch_total_params)
